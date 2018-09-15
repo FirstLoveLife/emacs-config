655a1f2572602f93870755400c889f0a31814e32
firstlove <c941968215@gmail.com>
2018-09-15 19:53:11 +0800

    misc

--- config.el
+++ config.el
@@ -2,7 +2,7 @@
 (load! "+bindings")
 (after! doom-themes
   :init
-                                        ;(setq doom-theme 'doom-tomorrow-day))
+  ;(setq doom-theme 'doom-tomorrow-day))
   (setq doom-theme 'doom-one))
 (after! helm
   (setq helm-source-list
@@ -16,54 +16,54 @@
           helm-source-locate
           helm-source-yasnippet
           )))
-(def-package! pyim
-  :demand t
-  :defer 10
-  :config
-  ;; 激活 basedict 拼音词库
-  (def-package! pyim-basedict
-    :config (pyim-basedict-enable))
+;; (def-package! pyim
+;;   :demand t
+;;   :defer 10
+;;   :config
+;;   ;; 激活 basedict 拼音词库
+;;   (def-package! pyim-basedict
+;;     :config (pyim-basedict-enable))
 
-  (setq default-input-method "pyim")
+;;   (setq default-input-method "pyim")
 
-  (require 'pyim-basedict) ; 拼音词库设置，五笔用户 *不需要* 此行设置
-  (pyim-basedict-enable)   ; 拼音词库，五笔用户 *不需要* 此行设置
-  (setq default-input-method "pyim")
+;;   (require 'pyim-basedict) ; 拼音词库设置，五笔用户 *不需要* 此行设置
+;;   (pyim-basedict-enable)   ; 拼音词库，五笔用户 *不需要* 此行设置
+;;   (setq default-input-method "pyim")
 
-  ;; 设置 pyim 探针设置，这是 pyim 高级功能设置，可以实现 *无痛* 中英文切换 :-)
-  ;; 我自己使用的中英文动态切换规则是：
-  ;; 1. 光标只有在注释里面时，我才可以输入中文。
-  ;; 2. 光标前是汉字字符时，才能输入中文。
-  ;; 3. 使用 M-j 快捷键，强制将光标前的拼音字符串转换为中文。
-  (setq-default pyim-english-input-switch-functions
-                '(
-                  pyim-probe-isearch-mode
-                  pyim-probe-program-mode
-                  pyim-probe-org-structure-template))
+;;   ;; 设置 pyim 探针设置，这是 pyim 高级功能设置，可以实现 *无痛* 中英文切换 :-)
+;;   ;; 我自己使用的中英文动态切换规则是：
+;;   ;; 1. 光标只有在注释里面时，我才可以输入中文。
+;;   ;; 2. 光标前是汉字字符时，才能输入中文。
+;;   ;; 3. 使用 M-j 快捷键，强制将光标前的拼音字符串转换为中文。
+;;   (setq-default pyim-english-input-switch-functions
+;;                 '(
+;;                   pyim-probe-isearch-mode
+;;                   pyim-probe-program-mode
+;;                   pyim-probe-org-structure-template))
 
-  (setq pyim-punctuation-translate-p '(no yes auto))   ;使用半角标点。
+;;   (setq pyim-punctuation-translate-p '(no yes auto))   ;使用半角标点。
 
-  (setq-default pyim-punctuation-half-width-functions
-                '(pyim-probe-punctuation-line-beginning
-                  pyim-probe-punctuation-after-punctuation))
+;;   (setq-default pyim-punctuation-half-width-functions
+;;                 '(pyim-probe-punctuation-line-beginning
+;;                   pyim-probe-punctuation-after-punctuation))
 
-  ;; 开启拼音搜索功能
-  (pyim-isearch-mode 1)
+;;   ;; 开启拼音搜索功能
+;;   (pyim-isearch-mode 1)
 
-  ;; 使用 pupup-el 来绘制选词框, 如果用 emacs26, 建议设置
-  ;; 为 'posframe, 速度很快并且菜单不会变形，不过需要用户
-  ;; 手动安装 posframe 包。
-  (setq pyim-page-tooltip 'posframe)
+;;   ;; 使用 pupup-el 来绘制选词框, 如果用 emacs26, 建议设置
+;;   ;; 为 'posframe, 速度很快并且菜单不会变形，不过需要用户
+;;   ;; 手动安装 posframe 包。
+;;   (setq pyim-page-tooltip 'posframe)
 
-  ;; 选词框显示5个候选词
-  (setq pyim-page-length 9)
+;;   ;; 选词框显示5个候选词
+;;   (setq pyim-page-length 9)
 
-  ;; 让 Emacs 启动时自动加载 pyim 词库
-  (add-hook 'emacs-startup-hook
-            #'(lambda () (pyim-restart-1 t)))
-  :bind
-  (("M-j" . pyim-convert-code-at-point) ;与 pyim-probe-dynamic-english 配合
-   ("C-;" . pyim-delete-word-from-personal-buffer)))
+;;   ;; 让 Emacs 启动时自动加载 pyim 词库
+;;   (add-hook 'emacs-startup-hook
+;;             #'(lambda () (pyim-restart-1 t)))
+;;   :bind
+;;   (("M-j" . pyim-convert-code-at-point) ;与 pyim-probe-dynamic-english 配合
+;;    ("C-;" . pyim-delete-word-from-personal-buffer)))
 
 
 (after! avy
@@ -191,16 +191,13 @@
   (add-hook 'haskell-mode-hook 'eglot-ensure)
   (add-hook 'ruby-mode-hook 'eglot-ensure)
   (add-hook 'python-mode-hook 'eglot-ensure)
-  (add-hook 'kotlin-mode-hook 'eglot-ensure)
-  :config
-(add-to-list 'eglot-server-programs '((haskell-mode) . (eglot-cquery "hie-wrapper"))))
-
+  (add-hook 'kotlin-mode-hook 'eglot-ensure))
 ;(add-hook 'haskell-mode-hook 'flycheck-mode)
-(add-to-list 'company-backends 'company-ghc)
+;(add-to-list 'company-backends 'company-ghc)
 (add-to-list 'company-backends 'company-nand2tetris)
 
-(set-company-backend! 'haskell-mode
-  'company-ghc)
+;(set-company-backend! 'haskell-mode
+;  'company-ghc)
 (set-company-backend! 'emacs-lisp-mode
   'company-elisp)
 (set-company-backend! 'nand2tetris-mode
