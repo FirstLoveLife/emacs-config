52ed442eb5baaaf7b607cb20421fa614f0c43fc7
firstlove <c941968215@gmail.com>
2018-09-17 14:56:47 +0800

    add custom.el

---
+++ custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(prog-mode-hook
+   (quote
+    (+version-control|git-gutter-maybe hl-todo-mode yas-minor-mode-on hl-line-mode highlight-escape-sequences-mode highlight-numbers-mode vi-tilde-fringe-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ccls-sem-global-variable-face ((t (:underline t :weight extra-bold))))
+ '(fixed-pitch ((t (:font "-*-Iosevka-*-*-*-*-16-*-*-*-*-*-*-*"))))
+ '(lsp-face-highlight-read ((t (:background "sea green"))))
+ '(lsp-face-highlight-write ((t (:background "brown4"))))
+ '(lsp-ui-sideline-current-symbol ((t (:foreground "grey38" :box nil))))
+ '(lsp-ui-sideline-symbol ((t (:foreground "grey30" :box nil)))))
