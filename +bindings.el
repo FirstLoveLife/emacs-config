d55c77aeee4eb8b92ff57c9a8f479ca03a0e93cd
firstlove <c941968215@gmail.com>
2019-04-18 14:59:29 +0800

    remove bingding

--- +bindings.el
+++ +bindings.el
@@ -72,46 +72,46 @@
  :inve "C-M-l" #'lsp
  )
 
-(map!
- :after ccls
- :localleader
- ;; $ccls/inheritance
- :desc "direct bases"  :n "b" (λ! (ccls/base 1))
- :desc "base 3" :n "B" (λ! (ccls/base 3))
- :desc "direct derived" :n "d" (λ! (ccls/derived 1))
- :desc "derived 3" :n "D" (λ! (ccls/derived 3))
+;; (map!
+;;  :after ccls
+;;  :localleader
+;;  ;; $ccls/inheritance
+;;  :desc "direct bases"  :n "b" (λ! (ccls/base 1))
+;;  :desc "base 3" :n "B" (λ! (ccls/base 3))
+;;  :desc "direct derived" :n "d" (λ! (ccls/derived 1))
+;;  :desc "derived 3" :n "D" (λ! (ccls/derived 3))
 
- (:desc "references" :prefix "r"
-   :desc "macro expansions" :n "m" #'ccls/references-macro
-   :desc "read" :n "r" #'ccls/references-read
-   :desc "write" :n "w" #'ccls/references-write
-   :desc "Call bit (e.g. where functions are taken addresses)" :n "n" #'ccls/references-not-call)
+;;  (:desc "references" :prefix "r"
+;;    :desc "macro expansions" :n "m" #'ccls/references-macro
+;;    :desc "read" :n "r" #'ccls/references-read
+;;    :desc "write" :n "w" #'ccls/references-write
+;;    :desc "Call bit (e.g. where functions are taken addresses)" :n "n" #'ccls/references-not-call)
 
- :desc "base" :n "i" #'ccls-inheritance-hierarchy
- :desc "derived" :n "I" (λ! (ccls-inheritance-hierarchy t))
+;;  :desc "base" :n "i" #'ccls-inheritance-hierarchy
+;;  :desc "derived" :n "I" (λ! (ccls-inheritance-hierarchy t))
 
 
- ;; $ccls/call
- :desc "caller" :n "c" #'ccls/caller
- :desc "callee":n "C" #'ccls/callee
- ;; caller hierarchy
- :desc "caller hierarchy" :n "e" #'ccls-call-hierarchy
- ;; callee hierarchy
- :desc "callee hierarchy" :n "E" (λ! (ccls-call-hierarchy t))
+;;  ;; $ccls/call
+;;  :desc "caller" :n "c" #'ccls/caller
+;;  :desc "callee":n "C" #'ccls/callee
+;;  ;; caller hierarchy
+;;  :desc "caller hierarchy" :n "e" #'ccls-call-hierarchy
+;;  ;; callee hierarchy
+;;  :desc "callee hierarchy" :n "E" (λ! (ccls-call-hierarchy t))
 
- ;; $ccls/member
- :desc "nested classes/namespace members" :n "s" (λ! (ccls/member 2))
- :desc "member functions / functions in a namespace" :n "f" (λ! (ccls/member 3))
- :desc "member variables" :n "m" (λ! (ccls/member 0))
- :desc "hierarchy" :n "M" #'ccls-member-hierarchy
+;;  ;; $ccls/member
+;;  :desc "nested classes/namespace members" :n "s" (λ! (ccls/member 2))
+;;  :desc "member functions / functions in a namespace" :n "f" (λ! (ccls/member 3))
+;;  :desc "member variables" :n "m" (λ! (ccls/member 0))
+;;  :desc "hierarchy" :n "M" #'ccls-member-hierarchy
 
- :desc "code-lens" :n "L" #'ccls-code-lens-mode
- :n "t" #'lsp-goto-type-definition
- ;; https://github.com/maskray/ccls/blob/master/src/messages/ccls_vars.cc#L15
- :desc "field or local variable" :n "v" (λ! (ccls/vars 3))
- :desc "field" :n "V" (λ! (ccls/vars 1))
- :desc "any" :n "C-v" (λ! (ccls/vars 7))
- )
+;;  :desc "code-lens" :n "L" #'ccls-code-lens-mode
+;;  :n "t" #'lsp-goto-type-definition
+;;  ;; https://github.com/maskray/ccls/blob/master/src/messages/ccls_vars.cc#L15
+;;  :desc "field or local variable" :n "v" (λ! (ccls/vars 3))
+;;  :desc "field" :n "V" (λ! (ccls/vars 1))
+;;  :desc "any" :n "C-v" (λ! (ccls/vars 7))
+;;  )
 
 (setcdr evil-insert-state-map nil)
 (define-key evil-insert-state-map (read-kbd-macro evil-toggle-key) 'evil-normal-state)
