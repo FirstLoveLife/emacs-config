3bd30d35da7aef4f095dfa785c86928f6480b0e8
Chenli <chenli@deepin.com>
2019-12-05 13:07:19 +0800

    use +ui

--- +ui.el
+++ +ui.el
@@ -1,7 +1,7 @@
 ;;; private/my/+ui.el -*- lexical-binding: t; -*-
 
-(setq doom-font (font-spec :family "Iosevka" :size 20))
-(setq doom-big-font (font-spec :family "Iosevka" :size 27))
+(setq doom-font (font-spec :family "Iosevka" :size 30))
+(setq doom-big-font (font-spec :family "Iosevka" :size 37))
 (remove-hook 'doom-init-ui-hook #'blink-cursor-mode)
 
 ;; (setq doom-theme 'doom-nord)
