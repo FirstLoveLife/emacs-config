8a191a3aa6c846cf6435beea754cf48c70b725ba
chenli <chenli@uniontech.com>
2020-09-29 20:07:18 +0800

    add default flag for ccls

--- modules/mylang/cc/config.el
+++ modules/mylang/cc/config.el
@@ -19,14 +19,14 @@
   (evil-set-initial-state 'ccls-tree-mode 'emacs)
   (setq ccls-initialization-options
         '(:compilationDatabaseDirectory "build"
-                                        :clang (:extraArgs ["-I/usr/include/qt" "-I/usr/include/libdtk-2.1.1/DCore" "/usr/include/qt/QtWidgets/"]
-                                                           :excludeArgs
-                                                           ;; Linux's gcc options. See ccls/wiki
-                                                           ["-falign-jumps=1" "-falign-loops=1" "-fconserve-stack" "-fmerge-constants" "-fno-code-hoisting" "-fno-schedule-insns" "-fno-var-tracking-assignments" "-fsched-pressure"
-                                                            "-mhard-float" "-mindirect-branch-register" "-mindirect-branch=thunk-inline" "-mpreferred-stack-boundary=2" "-mpreferred-stack-boundary=3" "-mpreferred-stack-boundary=4" "-mrecord-mcount" "-mindirect-branch=thunk-extern" "-mno-fp-ret-in-387" "-mskip-rax-setup"
-                                                            "--param=allow-store-data-races=0" "-Wa arch/x86/kernel/macros.s" "-Wa -"]
-                                                           )
-                                        :cache (:directory "/home/firstlove/tmp")
-                                        :index (:threads 8)
-                                        )
+          :clang (:extraArgs ["-I/usr/include/qt" "-I/usr/include/libdtk-2.1.1/DCore" "/usr/include/qt/QtWidgets/" "-std=c++20"]
+                  :excludeArgs
+                  ;; Linux's gcc options. See ccls/wiki
+                  ["-falign-jumps=1" "-falign-loops=1" "-fconserve-stack" "-fmerge-constants" "-fno-code-hoisting" "-fno-schedule-insns" "-fno-var-tracking-assignments" "-fsched-pressure"
+                   "-mhard-float" "-mindirect-branch-register" "-mindirect-branch=thunk-inline" "-mpreferred-stack-boundary=2" "-mpreferred-stack-boundary=3" "-mpreferred-stack-boundary=4" "-mrecord-mcount" "-mindirect-branch=thunk-extern" "-mno-fp-ret-in-387" "-mskip-rax-setup"
+                   "--param=allow-store-data-races=0" "-Wa arch/x86/kernel/macros.s" "-Wa -"]
+                  )
+          :cache (:directory "/home/firstlove/tmp")
+          :index (:threads 8)
+          )
         ))
